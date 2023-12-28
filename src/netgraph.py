1b7ebc24cd5447c73e1b8cb8068affe8f680dd7b
Andreas Gocht-Zech <andreas.gocht-zech@posteo.de>
2023-12-28 12:36:31 +0100

    refactor and simplify code a lot

--- src/netgraph.py
+++ src/netgraph.py
@@ -4,9 +4,9 @@ import signal
 import configparser
 import argparse
 import logging
-import dataclasses
 import datetime
-
+import queue
+import time
 
 import geo_data
 import regular_timer
@@ -25,29 +25,72 @@ class Singleton(type):
         return cls._instances[cls]
 
 
-@dataclasses.dataclass
-class Influx:
-    url: str
-    org: str
-    bucket: str
-    write_api: influxdb_client.WriteApi = None
+class Influx(threading.Thread):
+    def __init__(self, url, org, bucket, token, write_buffer_size = 1) -> None:
+        super().__init__()
 
-    def write(self, p: influxdb_client.Point):
-        self.write_api.write(bucket=self.bucket, org=self.org, record=p)
+        self.url = url
+        self.org = org
+        self.bucket = bucket
+        self.write_buffer_size = write_buffer_size
 
+        self.client = influxdb_client.InfluxDBClient(
+            url=url,
+            token=token,
+            org=org
+        )
 
-class NetGraphPcap(regular_timer.RegularTimer):
-    def __init__(self, influx: Influx, dt=datetime.timedelta(milliseconds=1000)):
-        super().__init__(dt)
-        self.influx = influx
+        self.write_api = self.client.write_api(write_options=SYNCHRONOUS)
+        self.write_buffer = []
+
+        self.queue = queue.Queue()
+        self.running = True
+
+    def send_data_point(self, elem):
+        self.queue.put(elem)
+
+    def stop(self):
+        self.running = False
+        self.join()
+
+    def run(self):
+        while self.running:
+            try:
+                if len(self.write_buffer) >= self.write_buffer_size:
+                    try:
+                        self.write_api.write(bucket=self.bucket, org=self.org, record=self.write_buffer)
+                        self.data_write_buffer = []
+                        logging.debug(f"send to Influx")
+                    except NewConnectionError as e:
+                        logging.error("Can't reach Influx, try again later")
+                        time.sleep(1)
+                        continue
+
+                try:
+                    elem = self.queue.get(timeout=1)
+                    logging.debug(f"Got elem")
+                except queue.Empty:
+                    continue
+                self.write_buffer.append(elem)
+
+            except Exception as e:
+                logging.exception("Some Exception occured, keep running")
+                
+
+class NetGraphPcap(regular_timer.RegularTimer, metaclass=Singleton):
+    def __init__(self, database: Influx, sample_rate: datetime.timedelta = datetime.timedelta(milliseconds=1000)):
+        super().__init__(sample_rate)
+        
+        logging.info(f"set sample_rate to {sample_rate}")
+        
+        self.database = database
         self.last_reported = nethogs.nethogs_packet_stats()
-        self.data_write_buffer = []
 
+    def update(self):
+        logging.debug("doing update")
 
-    def collect_data(self):
         current_stats = nethogs.nethogs_packet_stats()
         time = datetime.datetime.utcnow()
-        data_to_write = []
         for current, last in zip(current_stats, self.last_reported):
             if current.devicename != last.devicename:
                 logging.error(f"devices do not match: {current.device_name} != {last.device_name}")
@@ -58,59 +101,26 @@ class NetGraphPcap(regular_timer.RegularTimer):
             p.field("ps_drop", current.ps_drop - last.ps_drop)
             p.field("ps_ifdrop", current.ps_ifdrop - last.ps_ifdrop)
             p.time(time)
-            data_to_write.append(p)
+            self.database.send_data_point(p)
         self.last_reported = current_stats
-        return data_to_write
+    
+    def stop(self):
+        self.cancel()
+    
 
-    def send_data(self):
-        while len(self.data_write_buffer) > 0:
-            data = self.data_write_buffer.pop()
-            try:
-                self.influx.write(data)
-            except NewConnectionError as e:
-                self.data_write_buffer.append(data)
-                logging.error("Can't reach Influx, try again later")
-                break
+class NetGraphData(metaclass=Singleton):
+    def __init__(self, database: Influx, devices: list[str], geo_database: geo_data.GeoData, sample_rate: datetime.timedelta = datetime.timedelta(milliseconds=100)):
+        self.database = database     
 
-    def update(self):
-        try:
-            data_to_write = self.collect_data()
-            self.data_write_buffer.append(data_to_write)
-            self.data_to_wirte = []
-            self.send_data()
-        except Exception as e:
-            logging.exception("Some Exception occured")
-
-        
-
-
-class NetGraphData(regular_timer.RegularTimer, metaclass=Singleton):
-    def __init__(self, influx_config, devices, geo_database, dt=datetime.timedelta(milliseconds=1000)):
-        super().__init__(dt)        
-
-        self.influx = Influx(influx_config["url"], influx_config["org"], influx_config["bucket"])
-        self.pcap_to_ms = 100
+        self.pcap_to_ms = int(sample_rate/datetime.timedelta(milliseconds=1))
+        logging.info(f"set pcap_to_ms (sampling rate) to {self.pcap_to_ms} ms")
         self.devices = devices
         self.geo = geo_data.GeoData(geo_database)
-        self.pcap_stats = NetGraphPcap(self.influx)
-
-        token = influx_config["token"]
-
-        self.client = influxdb_client.InfluxDBClient(
-            url=self.influx.url,
-            token=token,
-            org=self.influx.org
-        )
-
-        self.influx.write_api = self.client.write_api(write_options=SYNCHRONOUS)
-        self.data_write_buffer = []
-
-        self.lock = threading.RLock()
-        self.data_to_wirte = []
 
     def callback(self, action: int, record: nethogs.NethogsMonitorRecord) -> None:
         time = datetime.datetime.utcnow()
         name = record.name
+        logging.debug("received callback")
 
         if record.pid == 0 and "-" in name:
             name = self.geo.check_and_translate(name)
@@ -128,10 +138,7 @@ class NetGraphData(regular_timer.RegularTimer, metaclass=Singleton):
         p.field("recv_bytes_last", record.recv_bytes_last)
         p.time(time)
 
-        with self.lock:
-            self.data_to_wirte.append(p)
-
-
+        self.database.send_data_point(p)
 
     def start(self):
         logging.info(f"Start Nethogs on the following devices: {self.devices}")
@@ -139,40 +146,18 @@ class NetGraphData(regular_timer.RegularTimer, metaclass=Singleton):
             target=nethogs.nethogsmonitor_loop_devices, args=(
                 self.callback, "", self.devices, False, self.pcap_to_ms))
         self.nethogs_th.start()
-        self.pcap_stats.start()
-        super().start()
 
     def stop(self):
         nethogs.nethogsmonitor_breakloop()
         self.nethogs_th.join()
-        self.pcap_stats.cancel()
-        self.cancel()
-
-    def send_data(self):
-        send_packets = 0
-        while len(self.data_write_buffer) > 0:
-            data = self.data_write_buffer.pop()
-            try:
-                self.influx.write(data)
-                send_packets += 1
-            except NewConnectionError as e:
-                self.data_write_buffer.append(data)
-                logging.error("Can't reach Influx, try again later")
-                break
-        logging.debug(f"send {send_packets} data packets")
-    
-    def update(self):
-        try:
-            with self.lock:
-                self.data_write_buffer.append(self.data_to_wirte)
-                self.data_to_wirte = []
-            self.send_data()
-        except Exception as e:
-            logging.exception("Some Exception occured")
 
 
 def main():
-    logging.basicConfig(level=logging.INFO)
+    import os
+
+    logging.basicConfig(
+        level=os.getenv('LOG_LEVEL', 'INFO').upper()
+        )
     
     parser = argparse.ArgumentParser(prog='NetGraph')
     parser.add_argument('-c', '--config', required=True)
@@ -186,23 +171,37 @@ def main():
     else:
         logging.info("loaded config file")
 
-    devices = [item.strip() for item in config["net_graph"]["interfaces"].split(",")]
-    geo_database = config["net_graph"].get("geo_database")
+    device_list = config.get("net_graph", "interfaces")
+    devices = [item.strip() for item in device_list.split(",")]
+    
+    geo_database = config.get("net_graph", "geo_database")
 
+    logging.info("setup Influx connection")
+    database = Influx(config.get("influx_db", "url"),
+                      config.get("influx_db", "org"),
+                      config.get("influx_db", "bucket"),
+                      config.get("influx_db", "token"), 
+                      config.getint("influx_db", "write_buffer_size", fallback = 1)
+                      )
+    database.start()
 
     logging.info("Configuring netgraph")
-    net_graph = NetGraphData(config["influx_db"], devices, geo_database)
+    sample_rate = datetime.timedelta(milliseconds=config.get("net_graph", "sampling_rate_ms", fallback=100))
+    net_graph_data = NetGraphData(database, devices, geo_database, sample_rate)
+    net_graph_pcap = NetGraphPcap(database, sample_rate)
 
     logging.info("Starting netgraph")
-    net_graph.start()
+    net_graph_data.start()
+    net_graph_pcap.start()
 
     def signal_handler(sig, frame):
         if sig == signal.SIGTERM or sig == signal.SIGINT:
-            net_graph.stop()
+            net_graph_data.stop()
+            net_graph_pcap.stop()
+            database.stop()
 
     signal.signal(signal.SIGINT, signal_handler)
     signal.sigwait([signal.SIGTERM, signal.SIGINT])
-
 
 if __name__ == '__main__':
     main()
